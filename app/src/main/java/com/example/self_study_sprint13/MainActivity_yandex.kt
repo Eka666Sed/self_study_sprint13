37ba93392a46b85c581eae4c23d035a8ed9a5dfd
Eka666Sed <mrs_sedova@mail.ru>
2023-10-03 13:46:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/self_study_sprint13/MainActivity_yandex.kt
@@ -0,0 +1,11 @@
+package com.example.self_study_sprint13
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity_yandex : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_yandex)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "self_study_sprint13"
+include(":app")
